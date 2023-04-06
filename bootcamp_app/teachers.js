46d2c5f7fe6c756ddd05d064d1edce5d28167a6d
NikJensen97 <npjjensen@hotmail.com>
2023-04-05 22:13:11 -0700

    SQL injection mod

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,18 @@ const client = new Client({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = '
 
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM assistance_requests
-JOIN teachers ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '%${process.argv[2]}%'
-ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
-.catch(err => console.error('query error', err.stack));
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher
+';
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values);
